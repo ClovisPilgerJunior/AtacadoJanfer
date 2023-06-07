bf3dc71ad1d67f21531dd8be76adfb2d38f54543
Clóvis Júnior <clovispilgerjunior@hotmail.com>
2023-06-07 11:49:56 -0300

    Criado a classe databaseConection
    Criado a logica e tabém uma estrutura para que possa utilizar a conexão mudando apenas os nomes e tipo de conxão.

--- src/main/java/model/DatabaseConnection.java
+++ src/main/java/model/DatabaseConnection.java
@@ -1,2 +1,25 @@
-package model;public class databaseConnection {
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class DatabaseConnection {
+
+    public Connection databaseLink;
+
+    public Connection getConnection(){
+        String databaseName = "janfer";
+        String databaseSchema = "atacado";
+        String databaseUser = "postgres";
+        String databasePassword = "postgres";
+        String url = "jdbc:postgresql://localhost:5432/" + databaseName + "?currentSchema=" + databaseSchema;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            databaseLink = DriverManager.getConnection(url, databaseUser, databasePassword);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return databaseLink;
+    }
 }
